d5dc9fcd105bc4651e4863b04a8df438a01fe9c7
Mauricio Rodríguez <mauriciorodriguez@ZCOBTMH100167-MauricioRodriguez.local>
2018-05-13 15:27:04 -0500

    make it bettercdsd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  root "tests#index" 
   resources :tests
   resources :steps, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
