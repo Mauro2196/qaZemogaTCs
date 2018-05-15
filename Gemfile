fbb7ba03bcb762cb43a0e2006df5d5f6cdfc7483
Mauricio Rodríguez <mauriciorodriguez@ZCOBTMH100167-MauricioRodriguez.local>
2018-05-15 15:59:41 -0500

    make it bettercdsddewr

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'bootstrap', '~> 4.1.0'
 gem 'jquery-rails'
+gem 'seed_dump'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 group :production do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {email: "mauro@mail.com", encrypted_password: "$2a$11$qOYyxJE1YFMtFL88Kk2VceYy7rSoXOQS93lJQRjB20b9.fucJBUD2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2018-05-13 18:47:21", last_sign_in_at: "2018-05-13 18:47:21", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
+])
+Step.create!([
+  {user_id: 1, test_id: 1, step: "dsdsd", expected_step: "ddsdsds"},
+  {user_id: 1, test_id: 1, step: "sddsdsd", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "asdad", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "asdad", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "sdfsf", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "sdsdsdsd", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "hola", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "sddsds", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "sddsds", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "ssd", expected_step: nil},
+  {user_id: 1, test_id: 1, step: "sdfsdfsf", expected_step: "sdfsdfsdfdsf"},
+  {user_id: 1, test_id: 1, step: "fffd", expected_step: "sdds"},
+  {user_id: 1, test_id: 1, step: "esfsd", expected_step: "dsf"}
+])
+Test.create!([
+  {user_id: 1, title: "Test 1", type_test: "functional", priority: "medium", sprint: "Sprint 1", Status: "un-tested", preconditions: "dsdfsdfsdfdsfdsfsdf"}
+])
